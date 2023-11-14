db32e0ab0bd6d2fffb0e6d39558cbb3188db6da9
Elderly Pig <Elderly Pig@ELDERLYPIG>
2023-11-14 20:38:42 +0300

    Добавьте файлы проекта.

---
+++ Лаба 1 С++ Решение.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+const double cost = 2.5;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Цена за 1 кг яблок 2.5 рубля"<<endl;
+	cout << "Введите количество килограмм"<<endl;
+	double weight;
+	cin >> weight;
+	if (weight < 0) {
+		cout << "Количество килограмм не может быть отрицательным";
+	}
+	else {
+		cout << "C Вас:"<< weight * cost;
+	}
+}
